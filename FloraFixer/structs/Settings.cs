32ef6a08dec5d62e5b499ce046720445c79f7811
Minizbot2012 <bradleyfwin@gmail.com>
2022-02-27 17:52:05 -0500

    Add more fixes; credit @TokcDK added as options

---
+++ FloraFixer/structs/Settings.cs
@@ -0,0 +1,8 @@
+namespace FloraFixer.Settings {
+    // Class is based upon TokcDK, but with extra tweaks :)
+    public class FloraFixSettings {
+        public string ScriptName = "florafix";
+        public bool FixProductions = true;
+        public bool FixActivateString = true;
+    }
+}

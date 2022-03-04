7b3b4464b23907008dbc8219c8b669530480db8d
Minizbot2012 <bradleyfwin@gmail.com>
2022-03-04 15:41:06 -0500

    UTF-8

--- FloraFixer/structs/Settings.cs
+++ FloraFixer/structs/Settings.cs
@@ -1,6 +1,8 @@
-namespace FloraFixer.Settings {
+namespace FloraFixer.Settings
+{
     // Class is based upon TokcDK, but with extra tweaks :)
-    public class FloraFixSettings {
+    public class FloraFixSettings
+    {
         public string ScriptName = "florafix";
         public bool FixProductions = true;
         public bool FixActivateString = true;
